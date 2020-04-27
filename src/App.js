6e8cbd5649fa45d04841f6f49fd58771c66aa882
Freddy Suarez <freddy.armando.suarez@gmail.com>
2020-04-26 22:46:45 -0300

    new

--- src/App.js
+++ src/App.js
@@ -12,42 +12,52 @@ import {
   NavLink
 } from "react-router-dom";
 
+//imports for using Tabla component
+import Tabla from './Components/Tabla';
 
+//imports for using NameForm component
+import NameForm from './Components/Nameform'
 
+//imports for using NameForm component
+import Peliculas from './Components/Movies'
 
 
 function App() {
   return (
     <Router>
-      <div  id="header">
-        <div><img src="my-app2/fs192.png"></img> </div>
+      <div id="header">
+        <div><img id="fslogo" src="my-app2/fs192.png"></img> </div>
         <nav>
           <ul>
             <li>
               <NavLink
                 exact
-                to="/" 
+                to="/"
                 activeClassName="active"
               > Home
               </NavLink>
-              
-            
+
+
             </li>
-            
+
             <li>
               <NavLink to="/signup" activeClassName="active">Sign up</NavLink>
             </li>
             <li>
               <NavLink to="/users" activeClassName="active">Users</NavLink>
             </li>
+            <li>
+              <NavLink to="/movies" activeClassName="active">Movies</NavLink>
+            </li>
           </ul>
         </nav>
-
+      </div>
+      <div id="main">
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-        <Route exact path="/" >
-          <Home />
+          <Route exact path="/" >
+            <Home />
           </Route>
           <Route path="/signup">
             <Signup />
@@ -55,35 +65,51 @@ function App() {
           <Route path="/users">
             <Users />
           </Route>
-         
+          <Route path="/movies">
+            <Movies moviecustomID="hola" />
+            <div
+              className="movies-footer"
+            >
+              <a
+                href="https://www.themoviedb.org/?language=es"
+                target="_blank"
+              >
+                <img
+                  src="my-app2/tmdb_logo.png"
+                >
+                </img>
+              </a>
+              <a
+                href="https://www.cinecalidad.is/"
+                target="_blank"
+              >
+                <img
+                  src="my-app2/cc_logo.png"
+                >
+                </img>
+              </a>
+
+
+            </div>
+          </Route>
+
         </Switch>
       </div>
     </Router>
+
+
   );
 }
 
 function Home() {
-  return  <div class="titulo"><h2>Home</h2></div> ;
+  return <h1>Home</h1>;
 }
 
 function Signup() {
   return <NameForm />;
 }
 
-
-function fetchData() {
-  
-  fetch('https://reactpostgresql.herokuapp.com/' + 'findall')
-      .then(response => response.json())
-      .then(json => manipularJson(json))
-  
-
-}
-
-let userArr;
-
-
-function manipularJson(json){
+/*function manipularJson(json){
   let divUser = document.querySelector("#userList");
   let ulUser = document.createElement("ul");
   ulUser.className= "user-list";
@@ -94,38 +120,72 @@ function manipularJson(json){
     liUser.textContent = element.firstName;
     ulUser.appendChild(liUser);
   });
-  divUser.appendChild(ulUser)
-  
-  
+  divUser.appendChild(ulUser);
+
+}*/
+let employeesJSON;
+function handleClick() {
+  fetch('https://reactpostgresql.herokuapp.com/' + 'findall')
+    .then(response => {
+      employeesJSON = response.json();
+    })
 }
 
-const numbers = [1, 2, 3, 4, 5];
-const listItems = numbers.map((number) =>
-  <li>{number}</li>
-);
 
-
-
-
-function handleClick(){
-  fetchData();
-  
- }
 
 function Users() {
   return (
     <div className="titulo">
-        <button id="load"
-          onClick={handleClick}>
-          Load
-        </button>
-        <div id="userList"></div>
-            
-        
+      <div id="userList">
+        <Tabla />
+      </div>
+
+
     </div>
-  
-  );  
-} 
+
+  );
+}
+
+function Movies() {
+  return (
+    <div class="movies">
+      <h2> Movies from Cinecalidad and The Movie Data Base</h2>
+      <p>Here you'll find some good movies that I have watched recently!.</p>
+
+      <Peliculas
+        moviecustomID="443791"
+        moviecustomLink="https://www.cinecalidad.is/pelicula/amenaza-en-lo-profundo-2020-online-descarga/"
+      />
+      <Peliculas
+        moviecustomID="522162"
+        moviecustomLink="https://www.cinecalidad.is/pelicula/midway-batalla-en-el-pacifico-2019-online-descarga/"
+      />
+      <Peliculas
+        moviecustomID="512200"
+        moviecustomLink="https://www.cinecalidad.is/pelicula/jumanji-el-siguiente-nivel-2019-online-descarga/"
+      />
+      <Peliculas
+        moviecustomID="492188"
+        moviecustomLink="https://www.cinecalidad.is/pelicula/historia-de-un-matrimonio-2019-online-descarga/"
+      />
+      <Peliculas
+        moviecustomID="419704"
+        moviecustomLink="https://www.cinecalidad.is/pelicula/ad-astra-hacia-las-estrellas-2019-online-descarga/"
+      />
+      <Peliculas
+        moviecustomID="399579"
+        moviecustomLink="https://www.cinecalidad.is/pelicula/battle-angel-la-ultima-guerrera-2019-online-descarga/"
+      />
+      <Peliculas
+        moviecustomID="568091"
+        moviecustomLink="https://www.cinecalidad.is/pelicula/fractura-2019-online-descarga/"
+      />
+
+    </div>
+
+  )
+}
+
 
 
 
@@ -135,225 +195,6 @@ function Users() {
 
 //Finish router
 
-const regexName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
-const regexDni = /^((?!(0))[0-9]{7,8})$/;
-const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstname: '',
-      lastname: '',
-      dni: '',
-      email: '',
-      firstnameError: false,
-      lastnameError: false,
-      dniError: false,
-      emailError: false,
-      emptyFirstname: true,
-      emptyLastname: true,
-      emptyDni: true,
-      emptyEmail: true
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-
-  }
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-
-
-    switch (target.name) {
-      case "firstname":
-        this.setState({ firstname: value });
-        if(value){
-          this.setState({emptyFirstname: false})
-        }else{
-          this.setState({emptyFirstname: true})
-        }
-        if (!regexName.test(value)) {
-          this.setState({ firstnameError: true });
-        } else {
-          this.setState({ firstnameError: false });
-        }
-        break;
-      case "lastname":
-        this.setState({ lastname: value });
-        if(value){
-          this.setState({emptyLastname: false})
-        }else{
-          this.setState({emptyLastname: true})
-        }
-        if (!regexName.test(value)) {
-          this.setState({ lastnameError: true });
-        } else {
-          this.setState({ lastnameError: false });
-        }
-        break;
-      case "dni":
-        if (value.length > 8) {
-          const valor = value.substr(0, 8);
-          this.setState({ dni: valor });
-         
-          //alert(valor);
-
-        } else {  
-          this.setState({ dni: value });
-          if(value){
-            this.setState({emptyDni: false})
-          }else{
-            this.setState({emptyDni: true})
-          }
-          if (!regexDni.test(value)) {
-            this.setState({ dniError: true });
-          } else {
-            this.setState({ dniError: false });
-          }
-
-        }
-        break;
-      case "email":
-        this.setState({ email: value });
-        if(value){
-          this.setState({emptyEmail: false})
-        }else{
-          this.setState({emptyEmail: true})
-        }
-        if (!regexEmail.test(value)) {
-          this.setState({ emailError: true });
-        } else {
-          this.setState({ emailError: false });
-        }
-        break;
-      default:
-        //Declaraciones ejecutadas cuando ninguno de los valores coincide con el valor de la expresión
-        break;
-
-
-
-    }
-    
-    
-    
-
-
-  }
-
-  handleSubmit(event) {
-   
-
-    
-    if (this.state.firstnameError || this.state.lastnameError|| this.state.dniError|| this.state.emailError) {
-      alert('Please check, some data is not valid!');
-      event.preventDefault();
-
-    }else if (this.state.emptyFirstname||this.state.emptyLastname||this.state.emptyDni||this.state.emptyEmail){
-      alert('Please complete all fields');
-      event.preventDefault();
-    }else{
-      alert('Firstname: '+ this.state.firstname+ ', Lastname: '+this.state.lastname +', DNI: '+ this.state.dni+', Email: '+ this.state.email)
-    }
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <form 
-          onSubmit={this.handleSubmit}
-          noValidate
-        >
-          <div className="row">
-          <div className="col-25">
-            <label>
-              Firstname:
-  
-          </label>
-          
-          </div>
-          <div className="col-75 tooltip">
-            <span className =  {"tooltiptext " +(this.state.firstnameError === true ? "show" : "not-show")}>Please enter a valid first name</span>
-            <input
-              name="firstname"
-              className={(this.state.firstnameError === true ? "invalid" : "valid")}
-              type="text"
-              value={this.state.firstname}
-              placeholder="Darth"
-              onChange={this.handleInputChange} />
-          </div>
-          </div>
-          <div className="row">
-          <div className="col-25">
-          <label>
-            Lastname:
-          
-          </label>
-          
-          </div>
-          <div className="col-75 tooltip">
-          <span className =  {"tooltiptext " +(this.state.lastnameError === true ? "show" : "not-show")}>Please enter a valid last name</span>
-          <input
-              name="lastname"
-              className={(this.state.lastnameError === true ? "invalid" : "valid")}
-              type="text"
-              value={this.state.lastname}
-              placeholder="Vader"
-              onChange={this.handleInputChange} />
-
-          </div>
-          </div>
-          <div className="row">
-          <div className="col-25">
-          <label>
-            DNI:
-          
-          </label>
-          </div>
-          <div className="col-75 tooltip">
-
-          <span className =  {"tooltiptext " +(this.state.dniError === true ? "show" : "not-show")}>Please enter a valid DNI</span>
-          <input
-              name="dni"
-              className={(this.state.dniError === true ? "invalid" : "valid")}
-              type="number"
-              value={this.state.dni}
-              placeholder="66555444"
-              onChange={this.handleInputChange} />
-            
-              </div>
-              </div>
-              <div className="row">
-          <div className="col-25">
-          <label>
-            Email:
-          
-          </label>
-
-          
-          </div>
-          <div className="col-75 tooltip">
-          <span className =  {"tooltiptext " +(this.state.emailError === true ? "show" : "not-show")}>Please enter a valid email</span>
-          <input
-              name="email"
-              className={(this.state.emailError === true ? "invalid" : "valid")}
-              type="email"
-              value={this.state.email}
-              placeholder="thesithlord@galacticempire.com"
-              onChange={this.handleInputChange} />
-              </div>
-              </div>
-          <input type="submit" value="SEND" className="send" />
-        </form>
-        <div class="version"><p>v2.0.1</p></div>
-        
-      </div>
-    );
-  }
-}
 
 
 
